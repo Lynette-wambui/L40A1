01271470b379ddf4209b80804f2da8a5c10e6ed0
LYNETTE WAMBUI <lynettemuraga47@gmail.com>
2025-04-26 18:20:28 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,16 @@
+name = "Lynette"
+age = 23
+is_student = True
+weight = 43.5
+
+print("Name :", name)
+print("Data Type of Name is", type(name))
+
+print("Age :", age)
+print("Data Type of Age is", type(age))
+
+print("Is Student :", is_student)
+print("Data Type of Is Student is", type(is_student))
+
+print("Weight :", weight)
+print("Data Type of Weight is", type(weight))
